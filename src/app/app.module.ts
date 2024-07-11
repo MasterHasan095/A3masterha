7d7e01efe011d148e42fb1ebfcead15fbf703333
masterha <masterha@sheridancollege.ca>
2024-07-11 15:03:06 -0400

    Starting assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+
+//My imports
+
+import { MaterialModule } from './modules/material-ui.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +21,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/classmasterha.ts
@@ -0,0 +1,18 @@
+class A3Classmasterha{
+    fullName991698598: string;
+    id991698598: number;
+    email991698598: string;
+    login991698598: string;
+
+    constructor(
+        fullName991698598: string,
+        id991698598: number,
+        email991688598: string,
+        login991698598: string
+    ){
+        this.fullName991698598 = fullName991698598;
+        this.id991698598 = id991698598;
+        this.email991698598 = email991688598;
+        this.login991698598 = login991698598;
+    }
+}
